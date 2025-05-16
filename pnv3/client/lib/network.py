6bf095299f99e22c907999210df81aec2cbbaf7e
iiPython <ben@iipython.dev>
2025-05-16 15:18:40 -0500

    network controller

--- pnv3/client/lib/network.py
+++ pnv3/client/lib/network.py
@@ -1,14 +1,29 @@
 # Copyright (c) 2025 iiPython
 
 # Modules
+import aiohttp
+
+# Initialization
+API_VERSION = "1"
 
 # Connection handling
 class Connection:
     def __init__(self, base_url: str) -> None:
-        self.base_url = base_url
+        self.base_url = f"{base_url}/v{API_VERSION}"
 
     async def fetch(self, hostname: str, page: str) -> str | None:
-        return None  # TODO
+        if self._client is None:
+            self._client = aiohttp.ClientSession()
+
+        async with self._client.get(f"/{hostname}/page/{page}") as response:
+            if response.status == 404:
+                return None
+
+            return await response.text()
 
     async def search(self, query: str) -> list[str]:
-        return []  # TODO
+        if self._client is None:
+            self._client = aiohttp.ClientSession()
+
+        async with self._client.post("/search", json = {"query": query}) as response:
+            return (await response.json())["data"]
